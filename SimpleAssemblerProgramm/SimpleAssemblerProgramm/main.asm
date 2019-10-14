2e7833bfabb6cdc624d964b2f4038c48bbb8bdc6
Happe <bh8332s@ad.fh-aachen.de>
2019-10-14 18:59:44 +0200

    init

--- SimpleAssemblerProgramm/SimpleAssemblerProgramm/main.asm
+++ SimpleAssemblerProgramm/SimpleAssemblerProgramm/main.asm
@@ -4,7 +4,7 @@
 ; Created: 10.10.2018 10:55:20
 ; Author : ie4938e
 ;
-.include "m8def.inc"	; Prozessordefinitionen laden
+.include "m8def.inc"	; Prozessordefinitionen laden (RAMEND, DDR, PORTB)
 
 .org 0x000		; Reset Vector
 	rjmp start
@@ -18,7 +18,7 @@ start:
 
 	;PB0 PB1 als Output
 	ldi r16,0b00000011	;letzen beiden Leds als Output(1) -> wo im datasheet steht PB0/1 als LEDs
-	out DDRB, r16	;inhalt von r16 in DDRB schreiben -> eigentliche Daten werden auf Input-Bus gelegt Data-Direction-Register(um ausgelesen zu werden von Befehlen)
+	out DDRB, r16	;warum einmal DDRB & dann PORTB in main? - inhalt von r16 in DDRB schreiben -> eigentliche Daten werden auf Input-Bus gelegt Data-Direction-Register(um ausgelesen zu werden von Befehlen)
 				
 				;R16 = Arbeitsregister(allzweck)
 
@@ -34,3 +34,16 @@ main:
 delay:
 	nop
 	ret
+
+
+	/*
+	.include "m8def.inc"         ; Definitionsdatei für den Prozessortyp einbinden
+
+         ldi r16, 0xFF       ; lade Arbeitsregister r16 mit der Konstanten 0xFF
+         out DDRB, r16       ; Inhalt von r16 ins IO-Register DDRB ausgeben
+
+         ldi r16, 0b11111100 ; 0b11111100 in r16 laden
+         out PORTB, r16      ; r16 ins IO-Register PORTB ausgeben
+
+	ende:    rjmp ende           ; Sprung zur Marke "ende" -> Endlosschleife
+	/*
