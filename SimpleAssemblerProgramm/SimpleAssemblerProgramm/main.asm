330edcaf5ad09244108e313f298391f6ec0973ca
Happe <bh8332s@ad.fh-aachen.de>
2019-10-15 12:04:27 +0200

    sd

--- SimpleAssemblerProgramm/SimpleAssemblerProgramm/main.asm
+++ SimpleAssemblerProgramm/SimpleAssemblerProgramm/main.asm
@@ -6,8 +6,8 @@
 ;
 .include "m8def.inc"	; Prozessordefinitionen laden (RAMEND, DDR, PORTB)
 
-.org 0x000		; Reset Vector
-	rjmp start
+.org 0x000		; Reset Vector - PC resetten
+	rjmp start	;
 ;Rest der Tabelle bleibt ungenutzt
 
 start:
@@ -36,14 +36,4 @@ delay:
 	ret
 
 
-	/*
-	.include "m8def.inc"         ; Definitionsdatei für den Prozessortyp einbinden
-
-         ldi r16, 0xFF       ; lade Arbeitsregister r16 mit der Konstanten 0xFF
-         out DDRB, r16       ; Inhalt von r16 ins IO-Register DDRB ausgeben
-
-         ldi r16, 0b11111100 ; 0b11111100 in r16 laden
-         out PORTB, r16      ; r16 ins IO-Register PORTB ausgeben
-
-	ende:    rjmp ende           ; Sprung zur Marke "ende" -> Endlosschleife
-	/*
+	
