51d28b8963ac2043b86a8efe5dc5fbd98086397e
benni11833 <happe_benedikt@yahoo.de>
2019-10-18 14:00:48 +0200

    Prak2

--- Praktika/Prak2_arch/Prak2_arch/main.c
+++ Praktika/Prak2_arch/Prak2_arch/main.c
@@ -9,14 +9,69 @@
 #define F_CPU 1000UL
 #include <util/delay.h>
 
+/*
 int main(void)
 {
     DDRB |= (1 << PB0);	//PB0 als Ausgang setzten -> 1
 	
 	while(1){
-		PORTB ^= (1 << PB0); //PB0 als LED an/aus schalten xor - möglich ganzen Byte in PORTB zu schreiben? (PORTB = 0bxxxxxxxx)
+		PORTB ^= (1 << PB0); //PB0 als LED an/aus schalten xor - mï¿½glich ganzen Byte in PORTB zu schreiben? (PORTB = 0bxxxxxxxx)
 		_delay_ms(200);
 	}
 	return 0;
+}*/
+
+/*
+Ablauf: -> unelegant
+PB2 	PB1 	PB0
+0		0		1
+0		1		0
+1		0		0
+0		1		0
+_______
+0		0		1
+...
+
+Ablauf:	-> mittelelegant
+PB2		PB1		PB0
+0		0		0
+0		0		1
+0		1		1
+
+*/
+
+void unelegant(){
+		PORTB ^= (1 << PB0);		//			0	0	1
+		_delay_ms(200);
+		PORTB ^= (1 << PB1) | (1 << PB0);//		0	1	0
+		_delay_ms(200);
+		PORTB ^= (1 << PB2) | (1 << PB1);//		1 	0	0
+		_delay_ms(200);
+		PORTB ^= (1 << PB2) | (1 << PB1);//		0	1	0
+		_delay_ms(200);
+		PORTB ^= (1 << PB1);			//		0	0	0
 }
 
+void mittelelegant(){
+	static bool flag = true;
+	PORTB = 0x00;
+	if(flag)	//PB0 soll leuchten, PB1, PB2 nicht
+		PORTB ^= (1 << PB0);
+	else		//PB2 sol leuchten, PB1, PB0 nicht
+		PORTB ^= (1 << PB2);
+	_delay_ms(200);
+	PORTB = 0x00;
+	PORTB  |= (1 << PB1);
+	_delay_ms(200);
+}
+
+int main(void)
+{
+    DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB2);	//PB0 als Ausgang setzten -> 1
+	
+	while(1){						//PORTB		0	0	0
+		//unelegant();
+		mittelelegant();
+	}
+	return 0;
+}
