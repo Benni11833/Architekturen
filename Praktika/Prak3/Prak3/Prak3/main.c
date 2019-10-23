f7be5f50dadc046e2d38d12ebe5786f7a47464b2
Happe <bh8332s@ad.fh-aachen.de>
2019-10-23 11:16:03 +0200

    Prak3_anfang

---
+++ Praktika/Prak3/Prak3/Prak3/main.c
@@ -0,0 +1,46 @@
+/*
+ * Prak3.c
+ *
+ * Created: 23.10.2019 10:40:13
+ * Author : bh8332s
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 1000000UL
+#include <util/delay.h>
+
+void init(){
+	DDRB |= (1 << PB0) | (1 << PB1);
+	DDRD = 0;	//DDRD als Eingang
+	PORTD = 0xff;	//PullUps auf PORTD einschalten
+}
+
+int Taster1_pressed(){
+	if(PIND == (PIND ^ (1 << PD2)))	//fixen
+		return 1;
+	else
+		return 0;
+}
+
+int Taster2_pressed(){
+	if(PIND == 0xfa)
+		return 1;
+	else
+		return 0;
+}
+
+int main(void)
+{
+	init();
+	
+    while (1) 
+    {
+		if(Taster1_pressed()) //0xfb -> PD2 gedrueckt
+			PORTB |= (1 << PB0);
+		else if(Taster2_pressed())
+			PORTB |= (1 << PB1);
+		else
+			PORTB = 0;
+    }
+}
+
